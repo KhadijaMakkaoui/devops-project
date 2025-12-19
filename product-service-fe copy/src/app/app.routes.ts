f1dc55d5bf836a40e098cf5c387c6632343ef850
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-12-19 10:29:46 +0100

    rename frontend project

--- product-service-fe copy/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list';
-import { ProductFormComponent } from './components/product-form/product-form';
-
-export const routes: Routes = [
-  { path: '', component: ProductListComponent },
-  { path: 'create', component: ProductFormComponent },
-  { path: 'edit/:id', component: ProductFormComponent }
-];
