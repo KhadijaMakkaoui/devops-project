392745b853e5af32d82975bf0756bc8271a41c51
YassineChahounia <ychahbounia@gmail.com>
2026-01-08 22:22:44 +0100

    Test1

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -22,3 +22,10 @@ output "alb_dns_name" {
   value = module.ecs.alb_dns_name
 }
 
+output "ecs_cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs.service_name
+}
